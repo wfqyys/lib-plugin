2c0a7df46387c1d91eb87ad0dd2a63d6afa81f54
wfq3212592665 <3212592665@qq.com>
2023-06-09 23:39:24 +0800

    主体框架和帮助文件初稿

--- apps/reserve_seat.js
+++ apps/reserve_seat.js
@@ -4,10 +4,8 @@ import plugin from '../../../lib/plugins/plugin.js'
 import{ rulePrefix } from '../utils/common.js'
 import { readFileSync, writeFileSync } from 'fs';
 import path from 'path';
-// import { defaults } from 'request-promise';
-// import { Builder, By, Key, until } from 'selenium-webdriver';
-// import { Options } from 'selenium-webdriver/chrome';
-
+const login_url = 'https://cas.yangtzeu.edu.cn/authserver/login?service=https%3A%2F%2Fseat.yangtzeu.edu.cn%2Fremote%2Fstatic%2FcasAuth%2FgetServiceByVerifyTicket%2FcasLogin';
+   
 
 //导出  类  类名===文件名 继承  插件类  
 export class Reserve extends plugin {
@@ -29,21 +27,83 @@ export class Reserve extends plugin {
                 //正则表达试
                 reg: `^${rulePrefix}(位置|座位)?(登录|登陆)$`,
                 //函数
-                fnc: 'login'
-            }
+                fnc: 'verify'
+                },
+                {
+                    //正则表达试
+                    reg: `^${rulePrefix}(账号|学号)?(绑定|添加)?20[0-9]{7}$`,
+                    //函数
+                    fnc: 'BindId'
+                },
+                {
+                    reg: `^${rulePrefix}帮助$`,
+                    fnc: 'help'
+                },
           ]
       });
   };
+// 帮助
+  async help (e) {
+    await runtimeRender(e, '/help/help.html')
+  }
  //函数
  async reserve(){
 
  }
- async login() {
+ async verify() {
     if (this.e.isGroup) {
-      this.e.reply('请私聊使用~')
+        this.e.reply('请私聊使用~')
         return true
     }else{
-        
+        this.e.reply('请发送"#lib+学号"进行绑定学号')
+        return false
     }
 };
+
+//学号绑定
+async BindId(){
+    let id = parseInt(this.e.msg.replace(/[^0-9]/gi, ''))
+    let user = this.e.user_id
+    let passwd = this.passwd
+    await redis.set(`lib-plugin:ID:${user}`, id)
+    await redis.set(`lib-plugin:PASSWD:${passwd}`, passwd)
+    this.reply('学号绑定成功', false)
+};
+
+// 取出用户的id,passwd
+async getData () {
+    let key = `lib-plugin:ID:${this.e.user_id}`
+    let ck = await getCk(this.e)
+    if (!ck) return false
+    if (await redis.get(key)) return false
+    let api = new MysSRApi('', ck)
+    let userData = await api.getData('srUser')
+    if (!userData?.data || _.isEmpty(userData.data.list)) return false
+    userData = userData.data.list[0]
+    let { game_id: gameid } = userData
+    await redis.set(key, gameid)
+    await redis.setEx(
+      `STAR_RAILWAY:userData:${gameid}`,
+      60 * 60,
+      JSON.stringify(userData)
+    )
+    return userData
+  }
+
+// 选择日期
+async chooseDate() {
+    const currentHour = new Date().getHours();
+    const currentDate = new Date().getDate();
+    return currentHour >= 21 ? currentDate + 1 : currentDate;
+  };
+
+  // 计算时间Id
+  async getTime(start_time, end_time) {
+    const start_time_hour = parseInt(start_time);
+    const end_time_hour = parseInt(end_time);
+    const start_num = start_time_hour % 1 === 0 ? start_time_hour * 60 : start_time_hour * 60 + 30;
+    const end_num = end_time_hour % 1 === 0 ? end_time_hour * 60 : end_time_hour * 60 + 30;
+    return [start_num, end_num];
+  };
+  
 }
